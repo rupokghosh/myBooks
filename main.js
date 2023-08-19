a332e5ea47db52a9e7caeecb2d46889d501c98bf
Rupok ghosh <rupokghosh291@gmail.com>
2023-08-19 13:35:55 -0700

    create basic function signatures

---
+++ main.js
@@ -0,0 +1,19 @@
+const myLibrary = [];
+const addButton = document.getElementById("addButton");
+const form = document.querySelector(".form");
+
+function Book(name, author, status, genre){
+    
+    this.name = name;
+    this.author = author;
+    this.status = status;
+    this.genre = status;
+}
+
+function addBookToLibrary() {
+
+}
+
+function makeForm(){
+
+}

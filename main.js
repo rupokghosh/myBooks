01206b7bec0397b61e5752756897435717e5bacd
rupokghosh <rupokghosh291@gmail.com>
2023-09-06 20:28:00 -0700

    add form details and constants

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const addButton = document.getElementById("addButton");
 const hiddenForm = document.getElementById("hiddenForm");
 const closeBtn = document.getElementById("close");
 const form = document.getElementById("myForm");
+const bookName = document.getElementById("name").value;
+const bookAuthor = document.getElementById("author").value;
+const bookGenre = document.getElementById("genre").value;
+const checkbox = document.getElementById("checkbox");
 
 function Book(name, author, status, genre){
     
@@ -13,7 +17,8 @@ function Book(name, author, status, genre){
 }
 
 function addBookToLibrary() {
-
+    const book = new Book();
+    myLibrary.push(Book);
 }
 
 // functions to show and hide the form
